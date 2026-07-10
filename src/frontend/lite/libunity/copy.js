a8ce3e66c6e4b836995749d97afa8d29656e1d2d
Arjan Bosboom <arjanbosboom@gmail.com>
2026-07-10 22:41:55 +0200

    Fix libunity downloader to use versioned release tags

--- src/frontend/lite/libunity/copy.js
+++ src/frontend/lite/libunity/copy.js
@@ -1,7 +1,9 @@
 var os = require("os");
+var packageJson = require("../package.json");
 
+let releaseTag = process.env.MUNT_RELEASE_TAG || `v${packageJson.version}`;
 let repo =
-  "https://github.com/muntorg/munt-official/releases/download/development/";
+  `https://github.com/muntorg/munt-official/releases/download/${releaseTag}/`;
 
 let file = "";
 if (os.platform() === "win32") {
@@ -39,4 +41,5 @@ const download = (uri, filename) => {
 const path = require("path");
 let src = repo + file;
 let dst = path.join(__dirname, `../src/unity/lib_unity.node`);
+console.log(`using release tag ${releaseTag}`);
 download(src, dst);
